a1b7976bb778b68947ec921dbf5421c4d3b035f9
budostylz <lewis.shaun1@outlook.com>
2018-03-18 22:23:11 -0400

    commit

---
+++ Calculus/Project/README.md
@@ -0,0 +1,16 @@
+## Closest Point on a Path and Its Tangent
+
+https://bl.ocks.org/andresin87/cb9c53581cdac51718d5
+
+##  Data-Driven Documents (D3 JavaScript)
+
+https://d3js.org
+
+## D3 Tutorial Table of Contents
+
+https://github.com/d3
+
+## D3.js Axes
+
+https://www.dashingd3js.com/d3js-axes
+
---
+++ Calculus/Project/rate_of_change.js
@@ -0,0 +1,34 @@
+﻿(function () {
+
+    window.onload = function () {
+
+        try {
+
+            //set SVG Viewport
+            var svg_dom = d3.select("body").append("svg")
+                .attr("width", 400)
+                .attr("height", 100);
+
+
+            //set scale
+            var _scale = d3.scale.linear()
+                .domain([0, 100])
+                .range([0, 400]);
+
+            //Set axis
+            var _x = d3.svg.axis()
+                .scale(_scale);
+
+            var x_group = svg_dom.append("g")
+                .call(_x);
+
+
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+
+   
+
+})();
