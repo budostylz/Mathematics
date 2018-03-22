2a627df602a43e5c62563db3cf1714c882425ca7
budostylz <lewis.shaun1@outlook.com>
2018-03-22 00:26:48 -0400

    commit

--- Calculus/Project/rate_of_change.js
+++ Calculus/Project/rate_of_change.js
@@ -4,24 +4,65 @@
 
         try {
 
-            //set SVG Viewport
-            var svg_dom = d3.select("body").append("svg")
-                .attr("width", 400)
-                .attr("height", 100);
+
+            function setGrid() {
+
+                // Set graph
+                var width = 700,
+                    height = 700,
+                    padding = 100;
+
+                // create an svg container
+                var vis = d3.select("#graph")
+                    .append("svg:svg")
+                    .attr("width", width)
+                    .attr("height", height);
+
+                var xScale = d3.scale.linear().domain([10, 0]).range([width - padding, padding]);
+                var yScale = d3.scale.linear().domain([-10, 10]).range([height - padding, padding]);
+
+                // define the y axis
+                var yAxis = d3.svg.axis()
+                    .orient("left")
+                    .scale(yScale);
+
+                // define the y axis
+                var xAxis = d3.svg.axis()
+                    .orient("bottom")
+                    .scale(xScale);
+
+                var xAxisPlot = vis.append("g")
+                    .attr("class", "axis axis--x")
+                    .attr("transform", "translate(0," + (height / 2) + ")")
+                    .call(xAxis.tickSize(-height, 0, 0));
+
+                var yAxisPlot = vis.append("g")
+                    .attr("class", "axis axis--y")
+                    .attr("transform", "translate(" + (width / 2) + ",0)")
+                    .call(yAxis.tickSize(-width, 0, 0));
 
 
-            //set scale
-            var _scale = d3.scale.linear()
-                .domain([0, 100])
-                .range([0, 400]);
+                xAxisPlot.selectAll(".tick line")
+                    .attr("y1", (width - (2 * padding)) / 2 * -1)
+                    .attr("y2", (width - (2 * padding)) / 2 * 1);
 
-            //Set axis
-            var _x = d3.svg.axis()
-                .scale(_scale);
+                yAxisPlot.selectAll(".tick line")
+                    .attr("x1", (width - (2 * padding)) / 2 * -1)
+                    .attr("x2", (width - (2 * padding)) / 2 * 1);
 
-            var x_group = svg_dom.append("g")
-                .call(_x);
 
+            }
+
+            //set cartesian plane
+            setGrid();
+
+            //set points on f(x)
+
+            //draw f(x)
+
+            //draw secant line
+
+            //draw tangent line
 
         }
         catch (e) {
