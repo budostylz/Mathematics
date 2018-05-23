9508378c7420d0a35f47f0b979fb365549eac55e
budostylz <lewis.shaun1@outlook.com>
2018-05-22 21:17:03 -0400

    commit

--- Calculus/Project/js/drag_drop.js
+++ Calculus/Project/js/drag_drop.js
@@ -15,7 +15,7 @@ function dragIt(e) {
 
     try {
         e.dataTransfer.setData("text", e.target.id);
-
+        //console.log("drag it")
     }
     catch (e) {
         console.log(e);
@@ -31,6 +31,22 @@ function dropIt(e) {
         var data = e.dataTransfer.getData("text");
         e.target.appendChild(document.getElementById(data));
 
+        var img = new Image();
+        var path = $('#drag1')[0].src
+        img.src = path;
+        img.setAttribute('crossOrigin', '');
+
+
+        var canvas = $('#canvas')[0];
+        var ctx = canvas.getContext("2d");//set context
+        ctx.drawImage(img, 0, 0);
+
+        var canvasImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        console.log(ctx.getImageData(0, 0, canvas.width, canvas.height));
+
+        console.log(canvas.height);
+
+
     }
     catch (e) {
         console.log(e);
@@ -38,4 +54,5 @@ function dropIt(e) {
 
 }
 
-console.log("drag drop");
+
+
