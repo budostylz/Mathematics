a23e48ab2c8571c2908ff590658b594eb7fbe43a
budostylz <lewis.shaun1@outlook.com>
2018-05-24 21:26:01 -0400

    commit

--- Calculus/Project/js/drag_drop.js
+++ Calculus/Project/js/drag_drop.js
@@ -1,58 +1,74 @@
-﻿function allowDrop(e) {
-
+﻿
+function drawRectangle(img) {
     try {
-        e.preventDefault();
-
-    }
-    catch (e){
-        console.log(e);
-    }
+        var canvasObj = $('#canvas');
+        var canvasElem = $('#canvas')[0];
 
 
-}
 
-function dragIt(e) {
+        //change width and length dynamically
+        //canvasElem.width = 300;
+        //canvasElem.height = 69;
 
-    try {
-        e.dataTransfer.setData("text", e.target.id);
-        //console.log("drag it")
+        //get canvas width and height
+        var x = canvasElem.width;
+        var y = canvasElem.height;
+
+        var ctx = canvasElem.getContext("2d");//set context
+
+        ctx.strokeRect(0, 0, 50, 150);
+        ctx.strokeRect(10, 0, 50, 150);
+        ctx.strokeRect(20, 0, 50, 150);
+        ctx.strokeRect(30, 0, 50, 150);
+        ctx.strokeRect(40, 0, 50, 150);
+        ctx.strokeRect(50, 0, 50, 150);
+        ctx.strokeRect(60, 0, 50, 150);
+        ctx.strokeRect(70, 0, 50, 150);
+        ctx.strokeRect(80, 0, 50, 150);
+        ctx.strokeRect(90, 0, 50, 150);
+        ctx.strokeRect(100, 0, 50, 150);
+
+        ctx.strokeRect(110, 0, 50, 150);
+        ctx.strokeRect(120, 0, 50, 150);
+        ctx.strokeRect(130, 0, 50, 150);
+        ctx.strokeRect(140, 0, 50, 150);
+        ctx.strokeRect(150, 0, 50, 150);
+        ctx.strokeRect(160, 0, 50, 150);
+        ctx.strokeRect(170, 0, 50, 150);
+        ctx.strokeRect(180, 0, 50, 150);
+        ctx.strokeRect(190, 0, 50, 150);
+        ctx.strokeRect(200, 0, 50, 150);
+
+        ctx.strokeRect(210, 0, 50, 150);
+        ctx.strokeRect(220, 0, 50, 150);
+        ctx.strokeRect(230, 0, 50, 150);
+        ctx.strokeRect(240, 0, 50, 150);
+        ctx.strokeRect(250, 0, 50, 150);
+
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.lineTo(300, 25);
+        ctx.stroke();
+
+
+
+
+
+        ctx.drawImage(img, 0, 50);
+
+
+
+
     }
     catch (e) {
-        console.log(e);
-    }
-
-
-}
-
-function dropIt(e) {
-
-    try {
-        e.preventDefault();
-        var data = e.dataTransfer.getData("text");
-        e.target.appendChild(document.getElementById(data));
-
-        var img = new Image();
-        var path = $('#drag1')[0].src
-        img.src = path;
-        img.setAttribute('crossOrigin', '');
-
-
-        var canvas = $('#canvas')[0];
-        var ctx = canvas.getContext("2d");//set context
-        ctx.drawImage(img, 0, 0);
-
-        var canvasImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        console.log(ctx.getImageData(0, 0, canvas.width, canvas.height));
-
-        console.log(canvas.height);
-
 
     }
-    catch (e) {
-        console.log(e);
-    }
-
 }
 
 
+var img = new Image();
+var path = $('#drag1')[0].src
+img.src = path;
 
+
+drawRectangle(img);
